6dd9e9b394fa0b3336b3ceae33b93ef766173950
gaelcervantes65 <oswaldo.cervantes7474@alumnos.udg.mx>
2023-03-31 15:56:52 -0600

    Mostrar errores de validacion en formularios

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,15 +5,15 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                    <a class="navbar-brand" href="{{ route('dashboard') }}">
+                        F+M estudio
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" style="text-decoration: none">
+                        Inicio
                     </x-nav-link>
                 </div>
             </div>
